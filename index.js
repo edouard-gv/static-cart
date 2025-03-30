45d214eb57bcef360882d32f89ccab589fbdf0b9
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2025-03-31 00:03:36 +0200

    Ebauche encodage panier

--- index.js
+++ index.js
@@ -12,4 +12,17 @@ window.addEventListener("load", async () => {
     document.addEventListener("addItemToCart", (event) => {
         shoppingCart.addItemToCart(event.detail);
     });
+
+    //Pour récupérer un panier à partir d'une URL
+    //
+
+    //Pour construire le code à partir du panier
+    //Si 1 x 001, 3 x 002 et 4 x 007 => 100130024007
+    //A convertir en hexa => 175036ea47
+    //A convertir en string en itérant avec String.fromCharCode sur chaque ? => '\x17\x50\x36\xea\x47'
+    //A convertir en base64 via btoa
+
+    //Inversement : atob
+    //on converti en hexa en itérant et appliquant sum (.chatAtCode(i) * 16^i)
+
 });
